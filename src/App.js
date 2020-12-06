188fd65e2d92994cdaa61a36089c1194ed59b5a8
Koras02 <wjdgh0727@gmail.com>
2020-12-06 23:06:53 +0900

    navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 import Navbar from './components/Navbar';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-
+import Section from "./components/Section";
+import dummyText from './DummyText';
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-       <div className="App">
-         <Navbar />
-         <Switch>
-           <Route exact path="/" component={Home}/>
-           <Route path="/About" component={About}/>
-           <Route path="/contact" component={Contact}/>
-         </Switch>
-       </div>
-      </BrowserRouter>
-    )
-  }
+  render(){
+  return (
+    <div className="App">
+      <Navbar />
+      <Section
+        title="Section 1"
+        subtitle={dummyText}
+        dark={true}
+        id="section1"
+        />
+      <Section
+        title="Section 2"
+        subtitle={dummyText}
+        dark={false}
+        id="section2"
+      />
+      <Section 
+        title="Section 3"
+        subtitle={dummyText}
+        dark={true}
+        id="section3"
+      />
+      <Section 
+        title="Section 4"
+        subtitle={dummyText}
+        dark={false}
+        id="section4"
+        />
+      <Section 
+        title="Section 5"
+        subtitle={dummyText}
+        dark={true}
+        id="section5"
+      />
+    </div>
+  );
+ }
 }
+
 export default App;
